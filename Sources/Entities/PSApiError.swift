3429fb84604927f0947f0d06ed584091a4626d72
Vytautas Gimbutas <vytautas@gimbutas.net>
2019-06-19 17:00:25 +0300

    Added PSApiError

---
+++ Sources/Entities/PSApiError.swift
@@ -0,0 +1,81 @@
+import Foundation
+import ObjectMapper
+
+public class PSApiError: Mappable, Error {
+    public var error: String?
+    public var statusCode: Int?
+    public var description: String?
+    public var properties: [String: Any]?
+    public var errors: [PSApiFieldError]?
+    
+    public init(error: String? = nil, description: String? = nil, statusCode: Int? = nil) {
+        self.error = error
+        self.description = description
+        self.statusCode = statusCode
+    }
+    
+    required public init?(map: Map) {
+    }
+    
+    public func mapping(map: Map) {
+        error       <- map["error"]
+        errors      <- map["errors"]
+        description <- map["error_description"]
+        properties  <- map["error_properties"]
+    }
+    
+    public func isUnauthorized() -> Bool {
+        return error == "unauthorized"
+    }
+    
+    public func isRefreshTokenExpired() -> Bool {
+        return error == "invalid_grant" && description == "Refresh token expired"
+    }
+    
+    public func isTokenExpired() -> Bool {
+        return error == "invalid_grant" && description == "Token has expired"
+    }
+    
+    public func isInvalidTimestamp() -> Bool {
+        return error == "invalid_timestamp"
+    }
+    
+    public func isNoInternet() -> Bool {
+        return error == "no_internet"
+    }
+    
+    class public func unknown() -> PSApiError {
+        return PSApiError(error: "unknown")
+    }
+    
+    class public func unauthorized() -> PSApiError {
+        return PSApiError(error: "unauthorized")
+    }
+    
+    public public class func mapping(json: String) -> PSApiError {
+        return PSApiError(error: "mapping", description: "mapping failed: \(json)")
+    }
+    
+    public public class func noInternet() -> PSApiError {
+        return PSApiError(error: "no_internet", description: "No internet connection")
+    }
+    
+    public public class func cancelled() -> PSApiError {
+        return PSApiError(error: "cancelled")
+    }
+}
+
+public class PSApiFieldError: Mappable {
+    public var code: String!
+    public var field: String!
+    public var message: String!
+    
+    required public init?(map: Map) {
+    }
+    
+    public func mapping(map: Map) {
+        code    <- map["code"]
+        field   <- map["field"]
+        message <- map["message"]
+    }
+}
